db7b3df0f65a90bf6bc2a0569c813017ba4e4320
Rishik Pathak <157512702+Rishik3104@users.noreply.github.com>
2025-08-18 22:40:02 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,107 @@
+import React from "react";
+import Waves from "./components/Waves";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ position: "relative" }}>
+      <Waves
+        lineColor="#fff"
+        backgroundColor="rgba(255, 255, 255, 0.2)"
+        waveSpeedX={0.02}
+        waveSpeedY={0.01}
+        waveAmpX={40}
+        waveAmpY={20}
+        friction={0.9}
+        tension={0.01}
+        maxCursorMove={120}
+        xGap={12}
+        yGap={36}
+      />
+      <header>
+        <div className="container">
+          <nav className="navbar navbar-expand-md" aria-label="Fourth navbar example" data-bs-theme="dark">
+            <div className="container-fluid">
+              <a href="/" className="navbar-brand d-inline-flex link-body-emphasis text-decoration-none">
+                <img src="img/android-icon-dark.png" alt="" height="30" />
+              </a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="nav">
+                <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#" hx-get="/index-htmx.html" hx-target="#main" id="btn-home">Home</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" target="_blank" href="#" hx-get="/blogs.html" hx-target="#main">Blog</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" target="_blank" href="https://scholar.google.com/citations?hl=en&user=3mS0Y4wAAAAJ">Research</a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Others</a>
+                    <ul className="dropdown-menu">
+                      <li><a className="dropdown-item" target="_blank" href="http://feynmanpi.com/">Feynman Pi</a></li>
+                      <li><a className="dropdown-item" target="_blank" href="https://www.linkedin.com/in/sagar-tmg/">LinkedIn</a></li>
+                      <li><a className="dropdown-item" target="_blank" href="https://www.youtube.com/@sagartamang0000/">YouTube</a></li>
+                      <li><a className="dropdown-item" target="_blank" href="https://sagartamang0000.wordpress.com/">Old Website</a></li>
+                    </ul>
+                  </li>
+                </ul>
+                <div className="col-md-3 text-end">
+                  <a href="projects/resume_latex_10_08_2025.pdf">
+                    <button type="button" className="btn btn-outline-primary me-2">Resume</button>
+                  </a>
+                  <a href="https://github.com/Rishik3104" target="_blank">
+                    <button type="button" className="btn btn-primary">GitHub</button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <video autoPlay muted loop className="hero-video">
+        <source src="img/hero-desktop-6.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <main id="main">
+        <div className="hero">
+          <section className="hero-content">
+            <div className="text-center py-5">
+              <h1 className="display-2 mb-3">
+                <span className="text-outline text-white">Rishik Pathak</span> 
+                <br />
+                <span className="text-ai-2 text-white display-6">Tech Enthusiast & Developer</span>
+              </h1>
+              <p className="lead text-white text-outline"><br /> and research.</p>
+              <div>
+                <button className="btn btn-primary me-2" hx-get="/contact.html" hx-target="#main" hx-indicator="#loader">Contact</button>
+                <button className="btn btn-outline-primary" hx-get="/projects.html" hx-target="#main" hx-indicator="#loader">
+                  Projects <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                </button>
+              </div>
+              <span className="htmx-indicator" id="loader">
+                <img src="img/loading3.gif" alt="loading..." height="50px" />
+              </span>
+            </div>
+          </section>
+        </div>
+      </main>
+      <script>
+        {`
+          const nav_btn = document.querySelector('.navbar-toggler');
+          const main = document.querySelector('main');
+          if(nav_btn) {
+            nav_btn.addEventListener('click', () => {
+              main.classList.toggle('main');
+            });
+          }
+        `}
+      </script>
+    </div>
+  );
+}
+
+export default App;
